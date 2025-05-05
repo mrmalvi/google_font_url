83cd7a19cb98a14ea37bd4c763dd80988b6a2514
mrmalvi <malviyak00@gmail.com>
2025-05-05 11:03:13 +0530

    url fix

--- lib/google_font_url/generator.rb
+++ lib/google_font_url/generator.rb
@@ -3,7 +3,7 @@ require_relative 'utils'
 module GoogleFontUrl
   class Generator
     def self.generate
-      GoogleFontUrl::Utils.read_fonts.map { |font_data| [font_data["family"], font_data["css_style_link"]] }
+      GoogleFontUrl::Utils.read_fonts.map { |font_data| [font_data["family"], font_data["css_url"]] }
     end
 
     def self.all
